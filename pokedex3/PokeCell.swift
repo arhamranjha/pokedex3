b733da36056607a37a5b52801651e41798e12b71
Arham Ranjha <arhamranjha@yahoo.com>
2016-12-21 18:19:22 -0800

    view created

---
+++ pokedex3/PokeCell.swift
@@ -0,0 +1,14 @@
+
+//
+//  PokeCell.swift
+//  pokedex3
+//
+//  Created by Arham Ranjha on 12/21/16.
+//  Copyright © 2016 Idea Centicity. All rights reserved.
+//
+
+import UIKit
+
+class PokeCell: UICollectionViewCell {
+    
+}
